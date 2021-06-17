810e57abc36b4b75365fbd7ff5f0c620b37c98a3
Milan Ganesh Acharya <ml650738@dal.ca>
2021-06-17 19:58:43 +0530

    Added fee assessment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import FeeAssessment from './courses/FeeAssessment'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This works !!!
-        </p>
-      </header>
+      <FeeAssessment />
     </div>
   );
 }
