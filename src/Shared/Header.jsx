df083938f849e07ff5af47bac667b7a828477063
Abdul Mueed Qadri <ab291996@dal.ca>
2021-06-21 22:59:13 +0530

    add nav menu

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -1,91 +1,91 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+import logo from '../images/Logo.png';
+import { Grid, Button, Toolbar, Avatar} from "@material-ui/core"
+
+import { Link} from "react-router-dom";
 
 
 const useStyles = makeStyles((theme) => ({
-    toolbar: {
-        minHeight: 68,
-        alignItems: 'flex-start',
-    },
-    logo:{
-        height: '5 em',
-        width: '11em',
-        marginTop: theme.spacing(1),
-    },
-    item: {
-        size: '10 em',
-    },
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-     },
-     title: {
-     flexGrow: 1,
-    },
+    
+  row:{
+    flexGrow:1
+  },
+  grow:{
+    flexGrow:1
+  },
+  container:{
+    width:1170,
+    margin:"auto"
+  },
+  buttonFontSize:{
+    fontSize:"11px",
+    color:"#a1a1a1"
+  },
+
+  AppBar:{
+    height:60,
+    backgroundColor:"#fff",
+    backgroundSize:"cover"
+  },
+  mainLogo:{
+    color: "#a1a1a1",
+    justifyContent:"left",
+    '&:hover':{
+      background:"transparent"
+    }
+  },
+
+  avatar:{
+    height:"120%",
+    borderRadius:0,
+  },
+
+  loginButton:{
+    background:"#e91e63",
+    color:"#fff",
+    borderRadius:"25px",
+    padding:"0px 25px",
+
+    '&:hover':{
+      background: 'blue',
+      boxShadow: "0px 2px 10px #888888"
+    }
+  }
 }))
     
 export default function Header(){
+      
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
 
-    function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-    }
-
-    const handleChange = (event, newValue) => {
-    setValue(newValue);
-    };
-    
-    return (
-        
+    return(
     <div className={classes.root}>
-    <AppBar>
-        <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-                CourseCompass
-            </Typography>
-                <Tabs value={value} onChange={handleChange} aria-label="navmenu">
-                        <Tab label="Courses" />
-                        <Tab label="Grades" />
-                        <Tab label="Schedule" />
-                        <Tab label="Profile" />
-                </Tabs>
-        </Toolbar>
-    </AppBar>
-    <TabPanel value={value} index={0}>
-        Item One
-    </TabPanel>
-    <TabPanel value={value} index={1}>
-        Item Two
-    </TabPanel>
-    <TabPanel value={value} index={2}>
-        Item Three
-    </TabPanel>    
+        <AppBar position="static" color="default" className={classes.AppBar}>
+        <Grid item sm={12} xs={12} className={classes.container}>
+            <Toolbar>
+            <Grid className={classes.grow}>
+                <Button className={[classes.mainLogo]}>
+                <Avatar src={logo} className={classes.avatar} />
+                </Button>
+            </Grid>
+            
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+                <Button color="inherit" className={classes.buttonFontSize}>Login</Button>
+            </Link>
+            <Link to="/profile" style={{ textDecoration: 'none' }}>
+                <Button color="inherit" className={classes.buttonFontSize}>Profile</Button>
+            </Link>
+            <Link to="/schedule" style={{ textDecoration: 'none' }}>
+                <Button color="inherit" className={classes.buttonFontSize}>Schedule</Button>
+            </Link> 
+            <Link to="/register" style={{ textDecoration: 'none' }}>
+                <Button color="inherit" className={classes.buttonFontSize}>Register</Button>
+            </Link> 
+            </Toolbar>
+        </Grid>
+        </AppBar>
     </div>
-    );  
+    )
 }
 
