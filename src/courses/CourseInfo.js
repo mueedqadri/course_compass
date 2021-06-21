f82da9b9c9de9523994ee76dd428c6d92042aca5
Milan Ganesh Acharya <ml650738@dal.ca>
2021-06-22 02:36:47 +0530

    Added Instructor info

--- src/courses/CourseInfo.js
+++ src/courses/CourseInfo.js
@@ -3,6 +3,14 @@ import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
 import Helmet from 'react-helmet';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import prof from '../images/professor.jpeg'
 
 function CourseInfo() {
     const courseColumns = [
@@ -22,17 +30,32 @@ function CourseInfo() {
     ]);
 
     const courseRows = [
-        { id: 'CSCI 5100', name: 'Communication Skills', credits: '3', mode: 'Online', timing: 'Mon: 08:00-09:30, Fri: 14:00-15:30', instructor: 'Martin Cash' }
+        { id: 'CSCI 5100', name: 'Intro to Computing', credits: '3', mode: 'Online', timing: 'Mon: 08:00-09:30, Fri: 14:00-15:30', instructor: 'Martin Cash' }
     ];
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            maxWidth: 345,
+        },
+        media: {
+            height: 0,
+            paddingTop: '56.25%', // 16:9
+        },
+        avatar: {
+            backgroundColor: 'red'
+        },
+    }));
+
+    const classes = useStyles();
 
     return (
         <div>
             <Helmet>
-                <title>Courses</title>
+                <title>Course Info</title>
             </Helmet>
             <Container>
                 <h1>Course Information</h1>
-                <Paper>
+                <Paper elevation='2'>
                     <Grid rows={courseRows} columns={courseColumns}>
                         <Table columnExtensions={courseTableExtensions} />
                         <TableHeaderRow />
@@ -40,9 +63,34 @@ function CourseInfo() {
                 </Paper>
                 <br />
                 <h1>Instructor Information</h1>
-                <Paper>
-                    
-                </Paper>
+                <Card className={classes.root} elevation='2'>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="instructor" className={classes.avatar}>
+                                M
+                            </Avatar>
+                        }
+                        title="Martin Cash"
+                        subheader="m.cash@coursecompass.com"
+                    />
+                    <CardMedia
+                        className={classes.media}
+                        image={prof}
+                        title="Paella dish"
+                    />
+                    <CardContent>
+                        <Typography variant="body1" color="textSecondary" component="p">
+                            {<div>
+                                <p>Martin Cash has PhD in Distributed Networking and Analysis. He has worked for a number of major network providers and 
+                                    has helped them with the development of various networking technologies.
+                                </p>
+                                <p>
+                                    Now he is persuing his passion for teaching, and helping research student with their thesis.
+                                </p>
+                            </div>}
+                        </Typography>
+                    </CardContent>
+                </Card>
             </Container>
         </div>
     );
