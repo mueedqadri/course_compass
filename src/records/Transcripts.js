9a7f7adee973d8aa7a019a24540341a5cfc31b12
manitejavarma <manitejavarma@dal.ca>
2021-07-03 23:31:13 -0300

    Add transcripts

--- src/records/Transcripts.js
+++ src/records/Transcripts.js
@@ -3,20 +3,28 @@ import countryList from "react-select-country-list";
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { PlayCircleFilledWhite } from '@material-ui/icons';
+import SendIcon from '@material-ui/icons/Send';
 
+import { Paper } from '@material-ui/core';
+
+import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
 const countries = countryList().getData();
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
+    TranscriptForm: {
+        background: 'white',
+        padding: theme.spacing(6),
+        margin: 'auto',
+        borderRadius: 10,
     },
-  }));
+    gridElements: {
+
+    }
+}));
 
 
 
@@ -31,74 +39,91 @@ function Transcripts() {
         setCountry(event.target.value);
     };
 
-    const submitRequest = ()=>{
+    const submitRequest = () => {
         alert("Request is submmited. If approved transcripts will be sent over mail.")
     };
 
-    const downloadTranscripts = ()=>{
+    const downloadTranscripts = () => {
         alert("Downloaded unofficial transcripts successfully")
     };
-  
+
     return (
-        <div style={{textAlign:'left'}}>
-            <h1 style={{textAlign:'center'}}>Request Official Transcripts</h1>
-            <br/>
-            <div style={{ backgroundColor:'white',borderRadius: 15, padding:'4rem'}}>
-            <form className={classes.root} noValidate autoComplete="off">
-                <Grid container spacing={1}>
-                    <Grid item xs={12}>
-                        <TextField id="outlined-basic" label="Name" variant="outlined" />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField id="outlined-basic" label="Number of Copies(Max 5)" variant="outlined" />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
+        <Container maxWidth="md" className={classes.root}>
+            <Typography component="div">
+                <Box fontWeight="fontWeightBold" fontSize="h5.fontSize" textAlign="center">Request Official Transcripts</Box>
+            </Typography>
+            <Box my="2rem" />
+            <Paper elevation={2} className={classes.TranscriptForm}>
+                <form noValidate autoComplete="off">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField id="outlined-basic" label="Name" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id="outlined-basic" label="Number of Copies (Max 5)" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
                                 id="outlined-multiline-static"
                                 label="Address"
                                 multiline
                                 rows={3}
                                 variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
+                            />
+                        </Grid>
+                        <Grid item xs={3} >
                             <TextField id="outlined-basic" label="City" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={3} >
                             <TextField id="outlined-basic" label="State/Province" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={3} >
                             <TextField id="outlined-basic" label="Zip/Postal Code" variant="outlined" />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            id="outlined-select-currency-native"
-                            select
-                            label="Select Country"
-                            value={country}
-                            onChange={handleChange}
-                            SelectProps={{
-                                native: true,
-                            }}
-                            helperText="Please select your country"
-                            variant="outlined"
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="outlined-select-currency-native"
+                                select
+                                label="Select Country"
+                                value={country}
+                                onChange={handleChange}
+                                SelectProps={{
+                                    native: true,
+                                }}
+                                helperText="Please select your country"
+                                variant="outlined"
                             >
-                            {countries.map((option) => (
-                                <option key={option.value} value={option.value}>
-                                {option.label}
-                                </option>
-                            ))}
-                        </TextField>
-                        
+                                {countries.map((option) => (
+                                    <option key={option.value} value={option.value}>
+                                        {option.label}
+                                    </option>
+                                ))}
+                            </TextField>
+
+                        </Grid>
+
                     </Grid>
-                       
-                </Grid> 
-                <Grid><Button variant="contained" color="primary" onClick={submitRequest}>Submit Request</Button></Grid>
-            </form>
-            </div>
-            <br/>
-            <br/>
-            <div>
-                <Button color="primary" size="medium" onClick={downloadTranscripts}>You can also download unofficial transcripts by clicking here</Button>
-            </div>
-            
-        </div>
+                    <Box my="2rem" />
+                    <Grid container spacing={1} justify="center">
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className={classes.button}
+                                endIcon={<SendIcon />}
+                                onClick={submitRequest}
+                                disableElevation
+
+                            >Send</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+            <Box my="3rem" />
+            <Typography variant="subtitle1" color="primary" style={{ display: 'flex' }}>
+                <Box color="red" mx={0.5} onClick={downloadTranscripts}>Click here</Box> if you want to downlod unofficial transcripts?
+            </Typography>
+        </Container >
     );
 }
 
