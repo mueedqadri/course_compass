d27058e1c52a3894c82c3804731f55e00174d73e
Abdul Mueed Qadri <ab291996@dal.ca>
2021-07-14 01:38:06 +0530

    update

--- src/components/records/Grades.jsx
+++ src/components/records/Grades.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SelectTerm from '../SelectTerm.js';
+import SelectTerm from '../Shared/SelectTerm';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
