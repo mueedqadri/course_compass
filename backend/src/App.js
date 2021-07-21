903f38de35a8e732b6f2e112361e5b0fefeed666
Abdul Mueed Qadri <ab291996@dal.ca>
2021-07-22 01:38:43 +0530

    Implemented routes and controllers to the backend api

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const mysql= require("mysql");
 const dbCredentials = require('./config/dbCredentials') 
+const courses = require('./routes/courseRegistration')
+const cors = require('cors')
+const courses = require('./routes/courseRegistration')
+
+
 
 let db = mysql.createConnection(dbCredentials);
 global.db = db;
 
 app.use(express.json());
 
-const courses = require('./routes/courseRegistration')
+
+app.use(cors())
 
 app.use('/', courses);
+app.use('/', users);
 
 app.use('/', (req, res)=>{
     res.send('User API')
--- backend/src/controllers/courseRegistration.js
+++ backend/src/controllers/courseRegistration.js
@@ -8,11 +8,17 @@ courseController.AllTerm = function (req, res) {
 };
 
 courseController.AllCourses = function (req, res) {
-    if ((req.params.termId || req.query.termId) && (req.params.depId || req.query.depId) ) {
-        const termId = req.params.termId ? req.params.termId : req.query.termId;
-        const depId = req.params.depId ? req.params.depId : req.query.depId;
-        let formattedDepIds = depId.split(',').map(i => `'${i}'`).join();
-        let sql = `SELECT c.*, it.*
+  if (
+    (req.params.termId || req.query.termId) &&
+    (req.params.depId || req.query.depId)
+  ) {
+    const termId = req.params.termId ? req.params.termId : req.query.termId;
+    const depId = req.params.depId ? req.params.depId : req.query.depId;
+    let formattedDepIds = depId
+      .split(",")
+      .map((i) => `'${i}'`)
+      .join();
+    let sql = `SELECT c.*, it.*, d.*
     FROM
         term_course AS tc
             JOIN
@@ -21,29 +27,26 @@ courseController.AllCourses = function (req, res) {
         instructor AS it ON it.instructorId = tc.instructorId
             JOIN
         course AS c ON c.courseId = tc.courseId
+            JOIN
+        department AS d ON d.departmentId = dc.departmentId
     WHERE
         tc.termId IN (${termId})
             AND dc.departmentId IN (${formattedDepIds})`;
-        
-        db.query(sql, (err, courses) => checkResults(
-            req,
-            res,
-            err,
-            courses,
-            "Courses found",
-            "Courses Not found"
-        ));
-    }
-    else {
-        res.status(400).json({
-            message: "Bad request",
-            success: false
-        });
-    }
+
+    db.query(sql, (err, courses) =>
+      checkResults(req, res, err, courses, "Courses found", "Courses Not found")
+    );
+  } else {
+    res.status(400).json({
+      message: "Bad request",
+      success: false,
+    });
+  }
 };
 
 courseController.AllDep = function (req, res) {
   let sql = `SELECT * FROM department;`;
+
   db.query(sql, (err, departments) =>
     checkResults(
       req,
@@ -56,11 +59,25 @@ courseController.AllDep = function (req, res) {
   );
 };
 
+courseController.GetCourse = function (req, res) {
+  if (req.params.courseId || req.query.courseId) {
+    const id = req.params.courseId ? req.params.courseId : req.query.courseId;
+    let sql = `SELECT * FROM course WHERE courseId = ${id}`;
+    db.query(sql, (err, course) =>
+      checkResults(req, res, err, course, "Course found", "Course Not found")
+    );
+  } else {
+    res.status(400).json({
+      message: "Bad request",
+      success: false,
+    });
+  }
+};
+
 courseController.GetUserCourses = function (req, res) {
-    if (req.params.uid || req.query.uid) {
-        const id = req.params.uid ? req.params.uid : req.query.uid;
-        let sql = 
-        `SELECT c.*, t.*,d.* FROM
+  if (req.params.uid || req.query.uid) {
+    const id = req.params.uid ? req.params.uid : req.query.uid;
+    let sql = `SELECT c.*, t.*,d.* FROM
               course AS c
                   INNER JOIN
               term_course AS tc ON c.courseId = tc.courseId
@@ -72,106 +89,122 @@ courseController.GetUserCourses = function (req, res) {
               department AS d ON d.departmentId = dc.departmentId
           WHERE
               c.courseId IN (SELECT courseId FROM user_course WHERE userid = ${id})`;
-      
-        db.query(sql, (err, courses) => checkResults(
-            req,
-            res,
-            err,
-            courses,
-            "User found",
-            "User Not found"
-        ));
-    }
-    else {
-        res.status(400).json({
-            message: "Bad request",
-            success: false
-        });
-    }
+
+    db.query(sql, (err, courses) =>
+      checkResults(req, res, err, courses, "User found", "User Not found")
+    );
+  } else {
+    res.status(400).json({
+      message: "Bad request",
+      success: false,
+    });
+  }
 };
 
 courseController.AddCourses = function (req, res) {
-        const body = req.body;
-        if (!(Object.keys(body).length === 2) || !body.hasOwnProperty('userId') || !body.hasOwnProperty('courseId')) {
-            return res.status(400).json({
-                success: false,
-                message: "Invalid request!"
-            });
-        }
-        let sql = `INSERT INTO user_course (userId, courseId) VALUES(${body["userId"]} ,${body["courseId"]} )`;
-        db.query(sql, function (err) {
-            try {
-                if (err) {
-                    throw err;
-                }
-                return res.status(201).json({
-                    success: true,
-                    message: "Course added!"
-                });
-            } catch (error) {
-                console.log(error)
-                return res.status(500).json({
-                    success: false,
-                    message: "Internal Server Error"
-                });
-            }
-        })
+  const body = req.body;
+  if (
+    !(Object.keys(body).length === 2) ||
+    !body.hasOwnProperty("userId") ||
+    !body.hasOwnProperty("courseId")
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid request!",
+    });
+  }
+  let sql = `INSERT INTO user_course (userId, courseId) VALUES(${body["userId"]} ,${body["courseId"]} )`;
+  db.query(sql, function (err) {
+    try {
+      if (err) {
+        throw err;
+      }
+      updateCourseCapacity(false, body["courseId"]);
+      return res.status(201).json({
+        success: true,
+        message: "Course added!",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
+  });
 };
 
+function updateCourseCapacity(isDrop, courseId){
+    let sql = `UPDATE course SET
+    filled = filled ${isDrop? '-': '+'} 1
+    WHERE  courseId = ${courseId};`;
+    db.query(sql, function (err, result) {
+      try {
+        if (err) throw err;
+      } catch (error) {
+          console.log(error)
+      }
+    });
+}
+
 courseController.DeleteCourses = function (req, res) {
-    if ((req.params.courseId || req.query.courseId) && (req.params.uid || req.query.uid) ) {
-        const uid = req.params.uid ? req.params.uid : req.query.uid;
-        const courseId = req.params.courseId ? req.params.courseId : req.query.courseId;
-        let sql = `DELETE FROM user_course WHERE userId = ${uid} AND courseId = ${courseId};`;
-        db.query(sql, function (err, result) {
-            try {
-                if (err) throw err;
-                if (result.affectedRows === 0) {
-                    return res.status(404).json({
-                        message: "record not found",
-                        success: false
-                    });
-                } else {
-                    return res.status(201).json({
-                        success: true,
-                        message: "Course deleted!"
-                    });
-                }
-            } catch (error) {
-                
-            }
-        })
-    }
-    else {
-        res.status(400).json({
-            message: "Bad request",
-            success: false
-        });
-    }
+  if (
+    (req.params.courseId || req.query.courseId) &&
+    (req.params.uid || req.query.uid)
+  ) {
+    const uid = req.params.uid ? req.params.uid : req.query.uid;
+    const courseId = req.params.courseId
+      ? req.params.courseId
+      : req.query.courseId;
+    let sql = `DELETE FROM user_course WHERE userId = ${uid} AND courseId = ${courseId};`;
+    db.query(sql, function (err, result) {
+      try {
+        if (err) throw err;
+        if (result.affectedRows === 0) {
+          return res.status(404).json({
+            message: "record not found",
+            success: false,
+          });
+
+        } else {
+            updateCourseCapacity(true, courseId);
+
+          return res.status(201).json({
+            success: true,
+            message: "Course deleted!",
+          });
+        }
+      } catch (error) {}
+    });
+  } else {
+    res.status(400).json({
+      message: "Bad request",
+      success: false,
+    });
+  }
 };
 
 function checkResults(req, res, err, objects, successMessage, failMessage) {
-    try{ 
+  try {
     if (err) throw err;
     if (!objects || !objects.length) {
-        res.status(404).json({
+      res.status(404).json({
         message: failMessage,
         success: false,
-        });
+      });
     } else {
-        res.status(200).json({
+      res.status(200).json({
         message: successMessage,
         success: true,
         data: objects,
-        });
+      });
+    }
+  } catch (ex) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
-    }
-    catch (ex) {
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        });
-    }
 }
 
 module.exports = courseController;
