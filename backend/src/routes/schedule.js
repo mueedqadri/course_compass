7f29c3e88acd4a236806a54d60e71f763f4a8eca
Milan Ganesh Acharya <ml650738@dal.ca>
2021-07-22 02:23:49 +0530

    Added APIs for schedule

---
+++ backend/src/routes/schedule.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router  = express.Router(); 
+
+const scheduleController = require('../controllers/schedule');
+router.get('/schedule/:id', scheduleController.userSchedule);
+
+module.exports = router; 
