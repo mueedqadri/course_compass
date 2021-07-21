9389ae705e6159a319ffb01ffb762c2963a812fc
Milan Ganesh Acharya <ml650738@dal.ca>
2021-07-22 04:16:48 +0530

    Modified schedule to fit the DB structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Shared/Footer';
 import Layout from './components/Shared/Layout';
 import { Switch, Route} from "react-router-dom";
 import ContentLayout from './components/Shared/ContentLayout'
-import Schedular from './components/Schedular/Schedular'
+import Schedule from './components/Schedule/Schedule'
 import ProfilePage from './components/Profile/ProfilePage'
 import Grades from './components/records/Grades';
 import Transcripts from './components/records/Transcripts';
@@ -38,10 +38,10 @@ export default function App(){
                             content = {<StudentDashboard/>}
                         />
                     </Route>
-                    <Route exact path="/schedular">
+                    <Route exact path="/schedule">
                         <ContentLayout
                             content = {
-                            <Schedular
+                            <Schedule
                                 height ={600}
                                 viewDefault ={'Month'}
                                 showToday ={true}
