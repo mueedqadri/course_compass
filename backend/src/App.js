16d1ec318f467a184069b50cfe916d66cb204471
Milan Ganesh Acharya <ml650738@dal.ca>
2021-07-22 01:11:11 +0530

    Fixed backend build issue

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const app = express();
 const mysql= require("mysql");
-const dbCredentials = require('./config/dbCredentials') 
-const courses = require('./routes/courseRegistration')
-const cors = require('cors')
-
+const dbCredentials = require('./config/dbCredentials'); 
+const courses = require('./routes/courseRegistration');
+const users = require('./routes/userManagement');
+const cors = require('cors');
 
 let db = mysql.createConnection(dbCredentials);
 global.db = db;
 
 app.use(express.json());
-
-<<<<<<< HEAD
-
-app.use(cors())
-const courses = require('./routes/courseRegistration')
-const users = require('./routes/userManagement')
-=======
->>>>>>> 82fc4273204605ab713de20c829e7af1043485ac
-
-app.use(cors)
+app.use(cors);
 app.use('/', courses);
 app.use('/', users);
 
