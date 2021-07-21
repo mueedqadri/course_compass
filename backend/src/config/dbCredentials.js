af74f9c3b64583dde77060a95f7f6c5d7afa5aff
Milan Ganesh Acharya <ml650738@dal.ca>
2021-07-22 01:25:36 +0530

    Removed credentials in js file

--- backend/src/config/dbCredentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = connectionDetails ={
-    host: "csci5709group9.cqiwyi4sn6kj.ap-south-1.rds.amazonaws.com",
-    user: "root",
-    password: "group9web",
-    database: "CourseCompass"
-}
