2701ff6fb54dd62940540fde78c1a0591b421b23
manitejavarma <manitejavarma@dal.ca>
2021-07-21 23:21:08 -0300

    add grades and transcripts

--- frontend/src/components/records/Grades.jsx
+++ frontend/src/components/records/Grades.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import SelectTerm from '../Shared/SelectTerm';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,63 +7,131 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Container from '@material-ui/core/Container';
+import axios from "axios";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
-
-
+//Styling for grade table
 const useStyles = makeStyles({
     table: {
-      minWidth: 650,
+        minWidth: 650,
     },
 });
-  
+
+//Method that is reused for populating grades state
 function createData(name, title, grade, attempted, earned) {
     return { name, title, grade, attempted, earned };
 }
-  
-const rows = [
-    createData('CSCI 5193', 'Technology Innovation', 'A+', 3.0, 3.0),
-    createData('CSCI 6505', 'Machine Learning', 'A+', 3.0, 3.0),
-    createData('CSCI 6509', 'Adv. Topics in NLP', 'A+', 3.0, 3.0),
-    createData('CSCI 9890', 'Internship Preparation', 'A+', 3.0, 3.0)
-];
 
 
 function Grades() {
-    const [term, setTerm] = useState("");
-    console.log(term);
+
     const classes = useStyles();
-  
+
+    //Various states used to get term and grades 
+    const [term, setTerm] = useState("");
+    const [terms, setTerms] = useState([]);
+    const [hasGrade, setHasGrade] = useState(false);
+    const [gradeRows, setRows] = useState([]);
+
+    //On change of term, update state and get grades 
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+        let termId = event.target.value;
+        getGrades(termId);
+    };
+
+    //method to get grades from backend based on termid
+    async function getGrades(termId) {
+        await axios.get("${process.env.REACT_APP_API_END_POINT}/grades/" + termId + "/1").then((res) => {
+
+            let rows = []
+            for (let resDataRow in res.data.data) {
+                let row = res.data.data[resDataRow]
+                rows.push(createData(row['courseCode'], row['title'], row['grade'], row['earnedcredits'], row['earnedcredits']))
+            }
+            setRows(rows)
+        });
+    }
+
+    //method invoked on load of page
+    const onLoadGrades = (termId) => {
+        getGrades(termId);
+    };
+
+    //method to get terms of students that contain grades
+    useEffect(() => {
+        async function getTerms() {
+            await axios.get("${process.env.REACT_APP_API_END_POINT}/grade_terms/1").then((res) => {
+                let Termlist = res.data.data
+                setTerms(res.data.data);
+                if (Termlist.length > 0) {
+                    setHasGrade(true);
+                }
+                setTerm(res.data.data[0]['termid']);
+                onLoadGrades(res.data.data[0]['termid']);
+            });
+        }
+        getTerms();
+    }, []);
+
+    // display "loading" until grades are populated
     return (
-        <div style={{textAlign:"center"}}>
-            <h1 style={{textAlign:"center"}}>Grades</h1>
-            <SelectTerm passTermData={setTerm} style={{textAlign:"left"}} />
-            <br/>
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell align="center">Course</TableCell>
-                        <TableCell align="center">Course Title</TableCell>
-                        <TableCell align="center">Final Grade</TableCell>
-                        <TableCell align="center">Credits Attempted</TableCell>
-                        <TableCell align="center">Credits Earned</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name}>
-                        <TableCell component="th" scope="row" align="center" >
-                            {row.name}
-                        </TableCell>
-                        <TableCell align="center">{row.title}</TableCell>
-                        <TableCell align="center">{row.grade}</TableCell>
-                        <TableCell align="center">{row.attempted}</TableCell>
-                        <TableCell align="center">{row.earned}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+        <div style={{ textAlign: "center" }}>
+            {(!hasGrade) ? <h1>Loading</h1> :
+                <div>
+                    <h1 style={{ textAlign: "center" }}>Grades</h1>
+                    <Container maxWidth="sm">
+                        <FormControl id="term-selector">
+                            <InputLabel shrink >Select Term</InputLabel>
+                            <Select
+                                value={term}
+                                displayEmpty
+                                onChange={handleChange}
+                            >
+                                {terms?.map((item) => {
+                                    return (
+                                        <MenuItem key={item['termid']} value={item['termid']}>
+                                            {item['term']}
+                                        </MenuItem>
+                                    );
+                                })}
+                            </Select>
+                        </FormControl>
+                    </Container>
+                    <br />
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Course</TableCell>
+                                    <TableCell align="center">Course Title</TableCell>
+                                    <TableCell align="center">Final Grade</TableCell>
+                                    <TableCell align="center">Credits Attempted</TableCell>
+                                    <TableCell align="center">Credits Earned</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {gradeRows.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row" align="center" >
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="center">{row.title}</TableCell>
+                                        <TableCell align="center">{row.grade}</TableCell>
+                                        <TableCell align="center">{row.attempted}</TableCell>
+                                        <TableCell align="center">{row.earned}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            }
+
         </div>
     );
 }
