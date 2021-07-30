db44b58578a5b75a17cce7406f98cb1ba14402d4
Philemon Lee <philemon.lee@dal.ca>
2021-07-30 09:07:24 -0300

    Changed status codes for authenticate

--- backend/src/controllers/userManagementController.js
+++ backend/src/controllers/userManagementController.js
@@ -39,18 +39,18 @@ userManagementController.authenticate = function (req, res) {
     console.log(emailId)
     console.log(password)
     const success = () =>
-        res.status(201).json({
+        res.status(200).json({
             message: "User authenticated",
             success: true,
             token: generateToken(emailId)
         })
     const invalidUser = () =>
-        res.status(201).json({
+        res.status(200).json({
             message: "User not found",
             success: false
         })
     const invalidPassword = () =>
-        res.status(404).json({
+        res.status(200).json({
             message: "Invalid password",
             success: false
         })
