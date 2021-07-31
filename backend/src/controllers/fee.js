81ca97dfc550accb48d3c6f560568dadd0b1ec16
Milan Ganesh Acharya <ml650738@dal.ca>
2021-07-31 05:47:35 +0530

    Fixed displaying of correct terms

--- backend/src/controllers/fee.js
+++ backend/src/controllers/fee.js
@@ -1,6 +1,45 @@
 // Developed by Milan Ganesh Acharya
 var feeController = {};
 
+//method to get terms that contains grades of a student
+feeController.courseTerm = function (req, res) {
+    try {
+        if ((req.params.userId || req.query.userId)) {
+            const userId = req.params.userId ? req.params.userId : req.query.userId;
+            let sql = `select distinct(term.termId),term from term, user_course, term_course where user_course.userId=${userId} and user_course.courseId=term_course.courseId and term_course.termId=term.termId`;
+
+            db.query(sql, function (err, term) {
+                if (err)
+                    throw err;
+                if (term.length) {
+                    res.status(200).json({
+                        success: true,
+                        courseInfo: term
+                    });
+                }
+                else {
+                    res.status(404).json({
+                        message: "Courses not found",
+                        success: false
+                    });
+                }
+            })
+        }
+        else {
+            res.status(400).json({
+                message: "Bad request",
+                success: false
+            });
+        }
+    }
+    catch (ex) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
 feeController.termFee = function (req, res) {
     try {
         if ((req.params.termId || req.query.termId) && (req.params.userId || req.query.userId)) {
--- backend/src/routes/fee.js
+++ backend/src/routes/fee.js
@@ -5,5 +5,6 @@ const router  = express.Router();
 
 const feeController = require('../controllers/fee');
 router.get('/fee/:termId/:userId', feeController.termFee);
+router.get('/course_terms/:userId', feeController.courseTerm);
 
 module.exports = router; 
